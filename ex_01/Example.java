1dddb03b2c30a5fd1f8a7aa6ddb8d3a776232046
xRezio <edwin.carin@epitech.eu>
2023-10-26 14:20:46 +0200

    reworked task6

---
+++ ex_01/Example.java
@@ -0,0 +1,12 @@
+package ex_01;
+
+public class Example {
+    public static void main(String[] args) {
+        Mars rocks = new Mars();
+        Mars lite = new Mars();
+        Mars snack = new Mars();
+        System.out.println(rocks.getId());
+        System.out.println(lite.getId());
+        System.out.println(snack.getId());
+    }
+}
